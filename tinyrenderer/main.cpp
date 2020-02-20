21abd80115f30d9c6c75f93048aab9660697815c
김준 <june324@snowcorp.com>
2020-02-20 20:35:21 +0900

    initial commit

---
+++ tinyrenderer/main.cpp
@@ -0,0 +1,14 @@
+#include "tgaimage.h"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red   = TGAColor(255, 0,   0,   255);
+
+int main(int argc, char** argv) {
+	TGAImage image(100, 100, TGAImage::RGB);
+	image.set(52, 41, red);
+	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+    std::string projectDIR = OUTPUT_DIR + static_cast<std::string>("output.tga");
+	image.write_tga_file(projectDIR.c_str());
+	return 0;
+}
+
